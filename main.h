fab66295ac7c7e3498190ad0ff6a55dbd6e9f654
enariyah <hopmichael52@gmail.com>
2022-08-08 08:23:46 -0700

    decimal

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+int decimal_print(int n);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_string(char *string);
